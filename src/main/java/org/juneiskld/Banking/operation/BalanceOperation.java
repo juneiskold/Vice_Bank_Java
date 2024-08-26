bdf00e6f4054e1e04a6fba2b367e7c3812843f80
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 00:47:56 +0200

    added execute method in balance operation

--- src/main/java/org/juneiskld/Banking/operation/BalanceOperation.java
+++ src/main/java/org/juneiskld/Banking/operation/BalanceOperation.java
@@ -1,4 +1,10 @@
 package org.juneiskld.Banking.operation;
 
+import org.juneiskld.Banking.account.Account;
+
 public class BalanceOperation {
+
+    public static double execute(Account account) {
+        return account.getBalance();
+    }
 }
