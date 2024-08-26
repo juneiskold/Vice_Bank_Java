81a8b8d1d0316c15828d87cc5ed629c08799ce87
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 00:41:22 +0200

    added execute method in deposit operation

--- src/main/java/org/juneiskld/Banking/operation/DepositOperation.java
+++ src/main/java/org/juneiskld/Banking/operation/DepositOperation.java
@@ -1,4 +1,12 @@
 package org.juneiskld.Banking.operation;
 
+import org.juneiskld.Banking.account.Account;
+import org.juneiskld.Banking.account.Transaction;
+
 public class DepositOperation {
+
+    public static void execute(Account account, double amount) {
+        account.setBalance(account.getBalance() + amount);
+        account.addTransaction(new Transaction("Deposit", amount));
+    }
 }
