dcc28d8cd12187ecd904a595b7276f2ee795bc54
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 01:09:06 +0200

    updated functionality for the transaction file

--- src/main/java/org/juneiskld/Banking/account/Transaction.java
+++ src/main/java/org/juneiskld/Banking/account/Transaction.java
@@ -1,4 +1,24 @@
 package org.juneiskld.Banking.account;
 
+import java.time.LocalDateTime;
+
 public class Transaction {
+
+    private LocalDateTime timestamp;
+
+    private String description;
+
+    private double amount;
+
+    public Transaction(String description, double amount) {
+
+        this.timestamp = LocalDateTime.now();
+        this.description = description;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s - R%.2f", timestamp, description, amount);
+    }
 }
