bcfa14c31c6408e86707a70763e4d5f70a12e6f8
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 00:43:58 +0200

    added execute method in withdrawal operation

--- src/main/java/org/juneiskld/Banking/operation/WithdrawOperation.java
+++ src/main/java/org/juneiskld/Banking/operation/WithdrawOperation.java
@@ -1,4 +1,17 @@
 package org.juneiskld.Banking.operation;
 
+import org.juneiskld.Banking.account.Account;
+import org.juneiskld.Banking.account.Transaction;
+
 public class WithdrawOperation {
+
+    public static boolean execute(Account account, double amount) {
+
+        if (account.getBalance() >= amount) {
+            account.setBalance(account.getBalance() - amount);
+            account.addTransaction(new Transaction("Withdrawal", -amount));
+            return true;
+        }
+        return false;
+    }
 }
