c4a88a07484d4d3921dcc4d5d171b75b4f3ab576
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 00:15:37 +0200

    initialized variables and constructor in the account file

--- src/main/java/org/juneiskld/Banking/Account.java
+++ src/main/java/org/juneiskld/Banking/Account.java
@@ -1,4 +1,23 @@
 package org.juneiskld.Banking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+
+    private String accountNumber;
+    private String ownerName;
+
+    private double balance;
+
+    private List<Transaction> transactions;
+
+    public Account(String accountNumber, String ownerName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = initialBalance;
+        this.transactions = new ArrayList<>();
+    }
+
+
 }
