28ea8298c979f92797a8701243e43fc63f9546f2
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 00:47:00 +0200

    added execute method in transfer operation

--- src/main/java/org/juneiskld/Banking/operation/TransferOperation.java
+++ src/main/java/org/juneiskld/Banking/operation/TransferOperation.java
@@ -1,4 +1,18 @@
 package org.juneiskld.Banking.operation;
 
+import org.juneiskld.Banking.account.Account;
+import org.juneiskld.Banking.account.Transaction;
+
 public class TransferOperation {
+
+    public static boolean execute(Account sender, Account recipient, double amount) {
+
+        if (WithdrawOperation.execute(sender, amount)) {
+            DepositOperation.execute(recipient, amount);
+            sender.addTransaction(new Transaction("Transfer to " + recipient.getAccountNumber(), -amount));
+            return true;
+
+        }
+        return false;
+    }
 }
