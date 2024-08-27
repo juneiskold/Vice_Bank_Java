6479aa2ce8058e60e1744802a314efc201f0ebaa
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 05:29:43 +0200

    removed complete functionality of the admin operations class

--- src/main/java/org/juneiskld/Banking/admin/AdminOperations.java
+++ src/main/java/org/juneiskld/Banking/admin/AdminOperations.java
@@ -2,62 +2,14 @@ package org.juneiskld.Banking.admin;
 
 import org.juneiskld.Banking.bank.Bank;
 
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AdminOperations {
 
+    private static final Logger logger = Logger.getLogger(AdminOperations.class.getName());
+
     public static void performAdminOperations(Scanner scanner, Bank bank) {
-        boolean authenticated = false;
-        String password;
-
-        while (!authenticated) {
-            System.out.print("Enter admin password: ");
-            password = scanner.nextLine();
-
-            if (AdminAuth.authenticate(password)) {
-                authenticated = true;
-                System.out.println("Authentication successful. Welcome, admin!");
-
-            } else {
-                System.out.println("Incorrect password. Try again.");
-            }
-        }
-
-        while (true) {
-
-            System.out.println("\nAdmin Operations:");
-            System.out.println("1. View all Account Numbers");
-            System.out.println("2. Delete Account");
-            System.out.println("0. Back to Main Menu");
-
-            System.out.print("`Choose an option: ");
-
-            try {
-                int choice = scanner.nextInt();
-                scanner.nextLine();
-
-                switch (choice) {
-
-                    case 1:
-                        ViewAccountsOperation.execute(bank);
-                        break;
-
-                    case 2:
-                        DeleteAccountOperation.execute(scanner, bank);
-                        break;
-
-                    case 0:
-                        System.out.println("Returning to Main Menu...");
-                        return;
-
-                    default:
-                        System.out.println("Invalid option. Please try again.");
-                }
-
-            } catch (Exception e) {
-                System.err.println("An unexpected error occurred: " + e.getMessage());
-            }
-        }
-    }
-}
