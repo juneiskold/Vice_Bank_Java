a221bdd3a8253875cf6b4226ad71daaa9633cf61
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 21:51:29 +0200

    added constructor in bank code that is initialized with the code attribute

--- src/main/java/org/juneiskld/Banking/bank/BankCode.java
+++ src/main/java/org/juneiskld/Banking/bank/BankCode.java
@@ -1,4 +1,18 @@
 package org.juneiskld.Banking.bank;
 
 public class BankCode {
+
+    private String code;
+
+    public BankCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
