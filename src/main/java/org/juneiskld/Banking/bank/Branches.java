a4ea9ff59e19ea0e8521f9adb05222d992e76b45
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 21:55:20 +0200

    made the constructo in branches class without a parameter and had get and set methods to initialize the branch attribute

--- src/main/java/org/juneiskld/Banking/bank/Branches.java
+++ src/main/java/org/juneiskld/Banking/bank/Branches.java
@@ -1,4 +1,21 @@
 package org.juneiskld.Banking.bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Branches {
+
+    private List<Branch> branches;
+
+    public Branches() {
+        this.branches = new ArrayList<>();
+    }
+
+    public void addBranch(Branch branch) {
+        this.branches.add(branch);
+    }
+
+    public List<Branch> getBranches() {
+        return branches;
+    }
 }
