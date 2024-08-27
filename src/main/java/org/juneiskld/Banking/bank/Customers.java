62ac7310084257f77c66f7170faaa69743296881
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 21:57:28 +0200

    added functionality to customers class in bank package

--- src/main/java/org/juneiskld/Banking/bank/Customers.java
+++ src/main/java/org/juneiskld/Banking/bank/Customers.java
@@ -1,4 +1,19 @@
 package org.juneiskld.Banking.bank;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customers {
+    private List<Customer> customers;
+
+    public Customers() {
+        this.customers = new ArrayList<>();
+    }
+
+    public void addCustomer(Customer customer) {
+        this.customers.add(customer);
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
 }
