89cda9857f8b13f7ebfb09f3c320317a9ea7a76c
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 10:45:38 +0200

    added exception to handle is if the account numbers are empty or null

--- src/main/java/org/juneiskld/Banking/admin/ViewAccountsOperation.java
+++ src/main/java/org/juneiskld/Banking/admin/ViewAccountsOperation.java
@@ -2,15 +2,20 @@ package org.juneiskld.Banking.admin;
 
 import org.juneiskld.Banking.account.Account;
 import org.juneiskld.Banking.bank.Bank;
-import org.juneiskld.Banking.utils.P
+import org.juneiskld.Banking.utils.PaginationHelper;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class ViewAccountsOperation {
 
     public static void execute(Bank bank) {
-
         List<String> accountNumbers = bank.getAllAccountNumbers();
+
+        if (accountNumbers == null || accountNumbers.isEmpty()) {
+            System.out.println("No accounts found in the bank.");
+            return;
+        }
         System.out.println("All Account Numbers:");
 
         for (String accountNumber : accountNumbers) {
