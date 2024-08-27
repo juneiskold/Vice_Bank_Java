0632a5ef9d88e69d464201be97855d9621c5b34e
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 20:30:29 +0200

    initialized account number, account holder, balance, account type, interest rate, creation date, branch code, currency and status

---
+++ src/main/java/org/juneiskld/Banking/bank_account/Account.java
@@ -0,0 +1,14 @@
+package org.juneiskld.Banking.bank_account;
+
+public class Account {
+
+    private String accountNumber;
+    private AccountHolder accountHolder;
+    private Balance balance;
+    private AccountType accountType;
+    private InterestRate interestRate;
+    private CreationDate creationDate;
+    private BranchCode branchCode;
+    private Currency currency;
+    private Status status;
+}
