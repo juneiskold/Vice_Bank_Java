6ed71475a94b89bc83f334f3bc95c1612c02283c
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 04:45:40 +0200

    added hashPassword method in adminAuth

--- src/main/java/org/juneiskld/Banking/admin/AdminAuth.java
+++ src/main/java/org/juneiskld/Banking/admin/AdminAuth.java
@@ -9,6 +9,21 @@ public class AdminAuth {
     private static final String HASHED_ADMIN_PASSWORD = "$2a$12$ZzKvTqyRtQxwWpBnFgYkYeOoJrGmLXhPbVlMqUuqzHcSfDlqzIiE";
 
     public static boolean authenticate(String password) throws NoSuchAlgorithmException {
-        return ADMIN_PASSWORD.equals(password);
+
+        String hashedPassword = hashPassword(password);
+        return HASHED_ADMIN_PASSWORD.equals(hashedPassword);
+    }
+
+    private static String hashPassword(String password)  throws NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hashBytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashBytes) {
+            sb.append(String.format("%02x", b));
+
+        }
+        return sb.toString();
     }
 }
