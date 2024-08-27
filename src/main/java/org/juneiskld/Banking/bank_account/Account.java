c9e36f8d61b67c8a89cffd61d2a81915a4b07808
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 20:32:56 +0200

    created the account constructor with the initialized attributes as parameters

--- src/main/java/org/juneiskld/Banking/bank_account/Account.java
+++ src/main/java/org/juneiskld/Banking/bank_account/Account.java
@@ -11,4 +11,19 @@ public class Account {
     private BranchCode branchCode;
     private Currency currency;
     private Status status;
+
+    public Account(AccountHolder accountHolder, Balance balance,
+                   AccountType accountType, InterestRate interestRate,
+                   CreationDate creationDate, BranchCode branchCode,
+                   Currency currency, Status status) {
+
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+        this.accountType = accountType;
+        this.interestRate = interestRate;
+        this.creationDate = creationDate;
+        this.branchCode = branchCode;
+        this.currency = currency;
+        this.status = status;
+    }
 }
