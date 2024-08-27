125dfeb1563b9371188d0a9c5ba81b9605d2cb41
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 20:46:26 +0200

    added the creation date constructor with the date being a parameter

--- src/main/java/org/juneiskld/Banking/bank_account/CreationDate.java
+++ src/main/java/org/juneiskld/Banking/bank_account/CreationDate.java
@@ -1,4 +1,12 @@
 package org.juneiskld.Banking.bank_account;
 
+import java.time.LocalDate;
+
 public class CreationDate {
+
+    private LocalDate date;
+
+    public CreationDate(LocalDate date) {
+        this.date = date;
+    }
 }
