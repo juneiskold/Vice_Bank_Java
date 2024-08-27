4ddac64ede05e934398e8f282590fc2aa1e39063
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 21:50:20 +0200

    initialized bank name in bank name class and added get and set methods

--- src/main/java/org/juneiskld/Banking/bank/BankName.java
+++ src/main/java/org/juneiskld/Banking/bank/BankName.java
@@ -1,4 +1,19 @@
 package org.juneiskld.Banking.bank;
 
 public class BankName {
+
+    private String name;
+
+    public BankName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
