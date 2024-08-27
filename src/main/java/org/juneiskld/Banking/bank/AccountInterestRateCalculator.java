359d4cbb4c244fb4458f4643c020499eff6ec383
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 11:10:35 +0200

    created constructor in the account type enum with cuurency and interest rate attributes

--- src/main/java/org/juneiskld/Banking/bank/AccountInterestRateCalculator.java
+++ src/main/java/org/juneiskld/Banking/bank/AccountInterestRateCalculator.java
@@ -1,4 +1,15 @@
 package org.juneiskld.Banking.bank;
 
+import java.math.BigDecimal;
+
 public interface AccountInterestRateCalculator {
+
+    /**
+     * Calculate the interest based on the account balance.
+     *
+     * @param balance the balance of the account
+     * @return the calculated interes
+     */
+
+    BigDecimal calculateInterest(BigDecimal balance);
 }
--- src/main/java/org/juneiskld/Banking/bank/AccountType.java
+++ src/main/java/org/juneiskld/Banking/bank/AccountType.java
@@ -1,8 +1,19 @@
 package org.juneiskld.Banking.bank;
 
-public enum AccountType {
+import java.math.BigDecimal;
+import java.util.Currency;
 
-    CHECKING,
-    SAVINGS,
-    MONEY_MARKET_ACCOUNT
+public enum AccountType implements AccountInterestRateCalculator {
+
+    CHECKING(Currency.getInstance("ZAR"), 0.01),
+    SAVINGS(Currency.getInstance("ZAR"), 0.02),
+    MONEY_MARKET_ACCOUNT(Currency.getInstance("ZAR"), 0.03);
+
+    private final Currency currency;
+    private final double interestRate;
+
+    AccountType(Currency currency, double interestRate) {
+        this.currency = currency;
+        this.interestRate = interestRate;
+    }
 }
