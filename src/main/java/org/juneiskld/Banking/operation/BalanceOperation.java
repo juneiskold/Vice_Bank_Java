9600eb09ab7700eac691b01b3f8de0814266eb69
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 12:27:58 +0200

    removed execute method in the balance operation and added a final logger attribute

--- src/main/java/org/juneiskld/Banking/operation/BalanceOperation.java
+++ src/main/java/org/juneiskld/Banking/operation/BalanceOperation.java
@@ -2,9 +2,16 @@ package org.juneiskld.Banking.operation;
 
 import org.juneiskld.Banking.account.Account;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.util.concurrent.CompletableFuture;
+
 public class BalanceOperation {
 
-    public static double execute(Account account) {
-        return account.getBalance();
-    }
+    private static final Logger logger = LoggerFactory.getLogger(BalanceOperation.class);
+
+
+
 }
