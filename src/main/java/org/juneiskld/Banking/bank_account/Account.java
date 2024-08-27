820bb84426ebccb5d5ea96398145f3f688588c90
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 20:36:46 +0200

    added name attribute to the account holder class

--- src/main/java/org/juneiskld/Banking/bank_account/Account.java
+++ src/main/java/org/juneiskld/Banking/bank_account/Account.java
@@ -26,4 +26,76 @@ public class Account {
         this.currency = currency;
         this.status = status;
     }
+
+//    public String getAccountNumber() {
+//        return accountNumber;
+//    }
+//
+//    public void setAccountNumber(String accountNumber) {
+//        this.accountNumber = accountNumber;
+//    }
+//
+//    public AccountHolder getAccountHolder() {
+//        return accountHolder;
+//    }
+//
+//    public void setAccountHolder(AccountHolder accountHolder) {
+//        this.accountHolder = accountHolder;
+//    }
+//
+//    public Balance getBalance() {
+//        return balance;
+//    }
+//
+//    public void setBalance(Balance balance) {
+//        this.balance = balance;
+//    }
+//
+//    public AccountType getAccountType() {
+//        return accountType;
+//    }
+//
+//    public void setAccountType(AccountType accountType) {
+//        this.accountType = accountType;
+//    }
+//
+//    public InterestRate getInterestRate() {
+//        return interestRate;
+//    }
+//
+//    public void setInterestRate(InterestRate interestRate) {
+//        this.interestRate = interestRate;
+//    }
+//
+//    public CreationDate getCreationDate() {
+//        return creationDate;
+//    }
+//
+//    public void setCreationDate(CreationDate creationDate) {
+//        this.creationDate = creationDate;
+//    }
+//
+//    public BranchCode getBranchCode() {
+//        return branchCode;
+//    }
+//
+//    public void setBranchCode(BranchCode branchCode) {
+//        this.branchCode = branchCode;
+//    }
+//
+//    public Currency getCurrency() {
+//        return currency;
+//    }
+//
+//    public void setCurrency(Currency currency) {
+//        this.currency = currency;
+//    }
+//
+//    public Status getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(Status status) {
+//        this.status = status;
+//    }
 }
--- src/main/java/org/juneiskld/Banking/bank_account/AccountHolder.java
+++ src/main/java/org/juneiskld/Banking/bank_account/AccountHolder.java
@@ -1,4 +1,11 @@
 package org.juneiskld.Banking.bank_account;
 
 public class AccountHolder {
+
+    private String name;
+
+    public AccountHolder(String name) {
+        this.name = name;
+    }
+
 }
