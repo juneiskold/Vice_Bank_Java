49907e2bde92893058403043f3d379dd41bfff77
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 10:09:24 +0200

    removed if statement in execute method in delete account operation

--- src/main/java/org/juneiskld/Banking/admin/DeleteAccountOperation.java
+++ src/main/java/org/juneiskld/Banking/admin/DeleteAccountOperation.java
@@ -5,21 +5,19 @@ import org.juneiskld.Banking.account.Account;
 
 
 import java.util.Scanner;
+import java.util.List;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DeleteAccountOperation {
+
+    private static final Logger logger = LoggerFactory.getLogger(DeleteAccountOperation.class);
 
     public  static void execute(Scanner scanner, Bank bank) {
 
         System.out.print("Enter account number to delete: ");
         String accountNumber = scanner.nextLine();
 
-        if (bank.deleteAccount(accountNumber)) {
-
-            System.out.println("Account " + accountNumber + " has been deleted.");
-
-        } else {
-            System.out.println("Account not found. Deletion failed.");
-        }
+        if
     }
 }
