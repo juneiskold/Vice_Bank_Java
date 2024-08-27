f4eaa9e9f529d934395ec9ec79edfa1b379e941a
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 02:38:56 +0200

    added code to update functionality for the admin authentication

--- src/main/java/org/juneiskld/Banking/admin/AdminAuth.java
+++ src/main/java/org/juneiskld/Banking/admin/AdminAuth.java
@@ -1,4 +1,10 @@
 package org.juneiskld.Banking.admin;
 
 public class AdminAuth {
+
+    private static final String ADMIN_PASSWORD = "admin123";
+
+    public static boolean authenticate(String password) {
+        return ADMIN_PASSWORD.equals(password);
+    }
 }
