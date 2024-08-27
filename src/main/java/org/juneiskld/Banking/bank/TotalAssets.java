b2c1a06621dd08495e2a9598221c9ff79ef6fd5d
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 21:56:27 +0200

    made a constructor in the total assets class

--- src/main/java/org/juneiskld/Banking/bank/TotalAssets.java
+++ src/main/java/org/juneiskld/Banking/bank/TotalAssets.java
@@ -1,4 +1,17 @@
 package org.juneiskld.Banking.bank;
 
 public class TotalAssets {
+    private double totalAssets;
+
+    public TotalAssets(double totalAssets) {
+        this.totalAssets = totalAssets;
+    }
+
+    public double getTotalAssets() {
+        return totalAssets;
+    }
+
+    public void setTotalAssets(double totalAssets) {
+        this.totalAssets = totalAssets;
+    }
 }
