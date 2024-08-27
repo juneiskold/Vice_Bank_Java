99d0347ac209a61da7fc0e8a4cc74e20bffbed5f
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 20:43:13 +0200

    created the interest rate constructor with the rate attribute as a parameter

--- src/main/java/org/juneiskld/Banking/bank_account/InterestRate.java
+++ src/main/java/org/juneiskld/Banking/bank_account/InterestRate.java
@@ -1,4 +1,10 @@
 package org.juneiskld.Banking.bank_account;
 
 public class InterestRate {
+
+    private double rate;
+
+    public InterestRate(double rate) {
+        this.rate = rate;
+    }
 }
