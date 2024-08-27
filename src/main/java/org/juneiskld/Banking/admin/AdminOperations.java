ea6515a4b2b21aedd52dd053f94168243126f0d1
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 02:50:41 +0200

    refactored files by haveing delete account and view account into their own files in the admin package

--- src/main/java/org/juneiskld/Banking/admin/AdminOperations.java
+++ src/main/java/org/juneiskld/Banking/admin/AdminOperations.java
@@ -1,4 +1,51 @@
 package org.juneiskld.Banking.admin;
 
+import org.juneiskld.Banking.bank.Bank;
+
+import java.util.Scanner;
+import java.util.List;
+
 public class AdminOperations {
+
+    public static void performAdminOperations(Scanner scanner, Bank bank) {
+
+        System.out.print("Enter admin password: ");
+        String password = scanner.nextLine();
+
+        if (!AdminAuth.authenticate(password)) {
+            System.out.println("Incorrect password. Access denied.");
+            return;
+        }
+
+        while (true) {
+
+            System.out.println("\nAdmin Operations:");
+            System.out.println("1. View all Account Numbers");
+            System.out.println("2. Delete Account");
+            System.out.println("0. Back to Main Menu");
+            System.out.print("`Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    viewAllAccountNumbers(bank);
+                    break;
+
+                case 2:
+                    deleteAccount(scanner, bank);
+                    break;
+
+                case 0:
+                    return;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+    private static void viewAllAccountNumbers(Bank bank) {
+
+    }
 }
