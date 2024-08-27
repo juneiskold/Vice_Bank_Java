b617e89e4f6b2bbae9a8bac002fc54375e16490a
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-27 05:17:50 +0200

    changed the imports on the adminAuth class by removing java security message, no such ans standard charset and replaced it with BCrypt

--- src/main/java/org/juneiskld/Banking/admin/AdminAuth.java
+++ src/main/java/org/juneiskld/Banking/admin/AdminAuth.java
@@ -1,8 +1,6 @@
 package org.juneiskld.Banking.admin;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.nio.charset.StandardCharsets;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class AdminAuth {
 
