88b92f450dfcaf63e43867c87d3bd76f8da14365
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 20:37:00 +0200

    added a database constructor withe the connection initialized

--- src/main/java/org/juneiskld/Banking/persistence/Database.java
+++ src/main/java/org/juneiskld/Banking/persistence/Database.java
@@ -1,4 +1,17 @@
 package org.juneiskld.Banking.persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
+
+    private final DatabaseConnection dbConnection;
+
+    public Database() {
+        this.dbConnection = new DatabaseConnection();
+    }
 }
