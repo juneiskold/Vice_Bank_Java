7186e8e8359f93475f670cfdf0002e61dbd3d741
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 20:58:07 +0200

    added a button constructor and extended it from jbutton

--- src/main/java/org/juneiskld/Banking/ui/Button.java
+++ src/main/java/org/juneiskld/Banking/ui/Button.java
@@ -1,4 +1,11 @@
 package org.juneiskld.Banking.ui;
 
-public class Button {
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+
+public class Button extends JButton {
+    public Button(String text, ActionListener listener) {
+        super(text);
+        addActionListener(listener);
+    }
 }
