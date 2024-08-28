8a5ed8ee37f6586e9bc662dab157b25ee7c1526d
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 15:52:14 +0200

    added the get all accounts method to return the list

--- src/main/java/org/juneiskld/Banking/admin/ViewAccountsOperation.java
+++ src/main/java/org/juneiskld/Banking/admin/ViewAccountsOperation.java
@@ -6,6 +6,20 @@ public class ViewAccountsOperation implements AdminOperations{
 
     @Override
     public void execute() {
+        System.out.println("Viewing all accounts:");
 
+        List<String> accounts = getAllAccounts();
+
+        for (String account : accounts) {
+            System.out.println(account);
+        }
+    }
+
+    private List<String> getAllAccounts() {
+
+        // this method should fetch all accounts from the database
+        // for now we'll return a dummy list
+
+        return List.of("Account1", "Account2", "Account3");
     }
 }
