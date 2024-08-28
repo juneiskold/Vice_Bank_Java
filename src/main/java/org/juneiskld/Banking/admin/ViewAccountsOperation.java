b62f56fe5f8540d5327582e1f8308aeb70a260ed
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 15:48:41 +0200

    had view accounts operation to implement admin operations

---
+++ src/main/java/org/juneiskld/Banking/admin/ViewAccountsOperation.java
@@ -0,0 +1,11 @@
+package org.juneiskld.Banking.admin;
+
+import java.util.List;
+
+public class ViewAccountsOperation implements AdminOperations{
+
+    @Override
+    public void execute() {
+
+    }
+}
