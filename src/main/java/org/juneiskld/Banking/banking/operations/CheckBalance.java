36c25813419bafa09d6b4398d51b62764d91f8e6
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 16:19:21 +0200

    initialized get balance method and hardcoded the balance returned

---
+++ src/main/java/org/juneiskld/Banking/banking/operations/CheckBalance.java
@@ -0,0 +1,10 @@
+package org.juneiskld.Banking.banking.operations;
+
+public class CheckBalance {
+
+    public double getBalance(String accountNumber) {
+
+        // logic to fetch balance from the account
+        return 0.0; // placeholder amount
+    }
+}
