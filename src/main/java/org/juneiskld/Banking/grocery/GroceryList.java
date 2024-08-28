7aa1c4aa86a8f209fbc958ec568cba21248880f1
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 20:01:34 +0200

    coded a grocery list constructor in the grocery list class

--- src/main/java/org/juneiskld/Banking/grocery/GroceryList.java
+++ src/main/java/org/juneiskld/Banking/grocery/GroceryList.java
@@ -1,4 +1,15 @@
 package org.juneiskld.Banking.grocery;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroceryList {
+
+    private List<GroceryItem> items;
+
+    public GroceryList() {
+        this.items = new ArrayList<>();
+    }
+
+
 }
