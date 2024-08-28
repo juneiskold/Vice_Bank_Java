53dffc9a0a7d663735f47d6a1058e3a296cd0d88
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 20:42:45 +0200

    code an execute query method in database class

--- src/main/java/org/juneiskld/Banking/persistence/Database.java
+++ src/main/java/org/juneiskld/Banking/persistence/Database.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Database {
 
@@ -13,5 +14,24 @@ public class Database {
 
     public Database() {
         this.dbConnection = new DatabaseConnection();
+    }
+
+    public <T> List<T> executeQuery(String sql, QueryExecutor<T> executor, Object... params) throws SQLException {
+        List<T> results = new ArrayList<>();
+
+        try (Connection conn = dbConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    results.add(executor.execute(rs));
+                }
+            }
+        }
+        return results;
     }
 }
