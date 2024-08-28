0c8fecb4e0e02d72ce61e1252aa04002b435051e
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 16:04:33 +0200

    added name, address, and phone number attributes to the account holder class still need to add constructors and getter and setter

--- src/main/java/org/juneiskld/Banking/banking/accounts/AccountHolder.java
+++ src/main/java/org/juneiskld/Banking/banking/accounts/AccountHolder.java
@@ -1,4 +1,10 @@
 package org.juneiskld.Banking.banking.accounts;
 
 public class AccountHolder {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    // constructor, getters and setters
 }
