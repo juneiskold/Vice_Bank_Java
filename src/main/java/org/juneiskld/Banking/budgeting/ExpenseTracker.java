d468b1ecddd10dca0dbf0eb3dad9067efb23a7ea
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 18:47:30 +0200

    added constructor method in the expense tracker class

--- src/main/java/org/juneiskld/Banking/budgeting/ExpenseTracker.java
+++ src/main/java/org/juneiskld/Banking/budgeting/ExpenseTracker.java
@@ -1,4 +1,13 @@
 package org.juneiskld.Banking.budgeting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpenseTracker {
+
+    private  List<Expense> expenses;
+
+    public ExpenseTracker() {
+        this.expenses = new ArrayList<>();
+    }
 }
