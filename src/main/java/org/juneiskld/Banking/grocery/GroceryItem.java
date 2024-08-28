535b5aa6a6a37bd9669f5f5e9bb9d57ea5ef0810
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 19:56:14 +0200

    added a get and set name in the grocery class

--- src/main/java/org/juneiskld/Banking/grocery/GroceryItem.java
+++ src/main/java/org/juneiskld/Banking/grocery/GroceryItem.java
@@ -11,4 +11,12 @@ public class GroceryItem {
         this.price = price;
         this.quantity = quantity;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
