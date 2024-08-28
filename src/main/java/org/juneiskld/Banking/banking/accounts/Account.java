771ad2722e096f3c0814e5545dcf577274f02ffb
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 16:01:36 +0200

    added the attributes of the account class

--- src/main/java/org/juneiskld/Banking/banking/accounts/Account.java
+++ src/main/java/org/juneiskld/Banking/banking/accounts/Account.java
@@ -1,4 +1,16 @@
 package org.juneiskld.Banking.banking.accounts;
 
 public class Account {
+
+    private String accountNumber;
+    private AccountHolder accountHolder;
+    private AccountType accountType;
+    private Balance balance;
+    private BranchCode branchCode;
+    private CreationDate creationDate;
+    private Currency currency;
+    private InterestRate interestRate;
+    private Status status;
+
+    // constructor, getters and setters
 }
