fa22f52c30a4698112063e60e01b45ec2ccc8101
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 19:54:29 +0200

    initialized attributsa nd constructor in the grocery class

--- src/main/java/org/juneiskld/Banking/grocery/GroceryItem.java
+++ src/main/java/org/juneiskld/Banking/grocery/GroceryItem.java
@@ -1,4 +1,14 @@
 package org.juneiskld.Banking.grocery;
 
 public class GroceryItem {
+
+    private String name;
+    private double price;
+    private int quantity;
+
+    public GroceryItem(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
