c6392bb8a98990610f17d2701f42aefbc208c990
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 16:23:25 +0200

    added calculate interest method that returns a double that has been hardcoded

--- src/main/java/org/juneiskld/Banking/banking/operations/InterestCalculation.java
+++ src/main/java/org/juneiskld/Banking/banking/operations/InterestCalculation.java
@@ -1,4 +1,10 @@
 package org.juneiskld.Banking.banking.operations;
 
 public class InterestCalculation {
+
+    public double calculateInterest(String accountNumber) {
+
+        // logic to calculate interest for the account
+        return 0.0;
+    }
 }
