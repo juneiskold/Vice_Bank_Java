d298699adddd4f8f83c26ba60d985d86697d079a
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 16:21:17 +0200

    created the deposit constructor method to the deposit class

---
+++ src/main/java/org/juneiskld/Banking/banking/operations/Deposit.java
@@ -0,0 +1,9 @@
+package org.juneiskld.Banking.banking.operations;
+
+public class Deposit {
+
+    public void deposit(String accountNumber, double amount) {
+
+        // logic to deposit amount into the account
+    }
+}
