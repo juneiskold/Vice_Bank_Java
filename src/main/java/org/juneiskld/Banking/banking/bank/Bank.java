59cd14764a15b8a1d6b405ca1d3b8f04e7aecd93
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 16:16:30 +0200

    coded bank attributes in the bank class still need to code constructors getters and setters

--- src/main/java/org/juneiskld/Banking/banking/bank/Bank.java
+++ src/main/java/org/juneiskld/Banking/banking/bank/Bank.java
@@ -1,7 +1,15 @@
 package org.juneiskld.Banking.banking.bank;
 
-import org.juneiskld.Banking.banking.*;
+import org.juneiskld.Banking.banking.accounts.Account;
+
+import java.util.List;
 
 public class Bank {
 
+    private BankCode bankCode;
+    private BankName bankName;
+    private Location location;
+    private List<Account> accounts;
+
+    // constructors, getters and setters
 }
