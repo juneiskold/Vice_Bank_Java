3ba010f172441e341ed2652bd5ee1662612fdd8e
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 19:01:00 +0200

    coded income calculator constructor and add income method

--- src/main/java/org/juneiskld/Banking/budgeting/IncomeCalculator.java
+++ src/main/java/org/juneiskld/Banking/budgeting/IncomeCalculator.java
@@ -1,4 +1,17 @@
 package org.juneiskld.Banking.budgeting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IncomeCalculator {
+
+    private List<Income> incomes;
+
+    public IncomeCalculator() {
+        this.incomes = new ArrayList<>()
+    }
+
+    public void addIncome(String source, double amount) {
+        incomes.add(new Income(source, amount));
+    }
 }
