b3c65a5a8af734e1bb68683efc5dfa307cadcbe8
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 15:57:41 +0200

    added a process transaction method to the transaction processor class

--- src/main/java/org/juneiskld/Banking/banking/BankAccountManager/TransactionProcessor.java
+++ src/main/java/org/juneiskld/Banking/banking/BankAccountManager/TransactionProcessor.java
@@ -1,4 +1,11 @@
 package org.juneiskld.Banking.banking.BankAccountManager;
 
 public class TransactionProcessor {
+
+    public void processTransaction(String fromAccount, String toAccount, double amount) {
+
+        System.out.println("Processing transaction of " + amount + " from " + fromAccount + " to " + toAccount);
+
+        // add logic to process the transaction
+    }
 }
