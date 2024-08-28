37e4e43123ff92f494241d16b0a7d383532db5c2
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 15:45:49 +0200

    implemented execute method

--- src/main/java/org/juneiskld/Banking/admin/DeleteAccountOperation.java
+++ src/main/java/org/juneiskld/Banking/admin/DeleteAccountOperation.java
@@ -7,4 +7,11 @@ public class DeleteAccountOperation implements AdminOperations {
     public DeleteAccountOperation(String accountNumber) {
         this.accountNumber = accountNumber;
     }
+
+    @Override
+    public void execute() {
+        System.out.println("Deleting account: " + accountNumber);
+
+        // add logic to delete the account from the system
+    }
 }
