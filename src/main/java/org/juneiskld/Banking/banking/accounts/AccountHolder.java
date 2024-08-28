e9e3b1e5c7c2fbe0c8dcd1afbd3db1aa2844bd17
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2024-08-28 16:02:51 +0200

    removed the code inside the account holder class

--- src/main/java/org/juneiskld/Banking/banking/accounts/AccountHolder.java
+++ src/main/java/org/juneiskld/Banking/banking/accounts/AccountHolder.java
@@ -1,19 +1,4 @@
 package org.juneiskld.Banking.banking.accounts;
 
 public class AccountHolder {
-
-    private String name;
-
-    public AccountHolder(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
